4f6ad7ef9d7c6e67135503d5f6c05a85a68f98f8
Adrian333Dev <adrian333dev@gmail.com>
2023-08-03 02:55:49 +0400

    custom user model added

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,48 @@
-from django.db import models # noqa
+# Database models
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import (
+    AbstractBaseUser, BaseUserManager, PermissionsMixin
+)
+
+
+class UserManager(BaseUserManager):
+    """Custom user manager that supports using email instead of username."""
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a new user."""
+        if not email:
+            raise ValueError('Users must have an email address.')
+        user = self.model(email=self.normalize_email(email), **extra_fields)
+        user.set_password(password)
+        # using=self._db is for supporting multiple databases
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password):
+        """Create and save a new superuser."""
+        user = self.create_user(email, password)
+        user.is_staff = True
+        user.is_superuser = True
+        # using=self._db is for supporting multiple databases
+        user.save(using=self._db)
+        return user
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    """Custom user model that supports using email instead of username."""
+
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    # This is the field that is used to authenticate
+    # the user when logging in
+    USERNAME_FIELD = 'email'
+
+    objects = UserManager()
+
+    def __str__(self):
+        """Return string representation of the user."""
+        return self.email
